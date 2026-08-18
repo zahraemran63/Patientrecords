abe4c530c6a64f8944a08127568fb2b64370631f
zahraemran63 <zahraemran63@gmail.com>
2026-08-18 18:55:15 +0300

    Fix Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  experimental: {
-    turbo: {
-      loaders: {
-        css: {
-          postcss: true,
-        },
-      },
-    },
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
